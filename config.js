9360185e3c6ca61f1e10c4c1a45bd097e9da8513
Astral-101 <73858679+Astral-101@users.noreply.github.com>
2021-08-30 19:49:18 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBeV-Hc2AQY5eSOOcjlk5v2avzeIA6jjlg",
+    authDomain: "c-85-57861.firebaseapp.com",
+    projectId: "c-85-57861",
+    storageBucket: "c-85-57861.appspot.com",
+    messagingSenderId: "947559574877",
+    appId: "1:947559574877:web:69a074a0d36ae6f65a4cb4",
+    databaseURL: "https://c-85-57861-default-rtdb.firebaseio.com/"
+  };
+
+  
